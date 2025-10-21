3c9f18566fc958107e8b690c870682a78416e6ef
Ugniu5v <ugniusvarzukas@gmail.com>
2025-10-21 13:31:56 +0300

    Bootstrap 5 ant visu page, concertu page pridejimas

--- concerts/urls.py
+++ concerts/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = "concerts"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create/", views.createConcert, name="createConcert"),
+    path("edit/", views.editConcert, name="editConcert"),
+    path("search/", views.searchConcert, name="searchConcert"),
 ]
--- concerts/views.py
+++ concerts/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "concerts/index.html")
+
+
+def createConcert(request):
+    return render(request, "concerts/createConcert.html")
+
+
+def editConcert(request):
+    return render(request, "concerts/editConcert.html")
+
+
+def searchConcert(request):
+    return render(request, "concerts/searchConcert.html")
