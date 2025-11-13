f1e1ce0557e3c13417a71aaf44bca353ba36a456
Gytis Kaulakis <42701017+GytisKau@users.noreply.github.com>
2025-11-13 16:04:04 +0200

    Pagrindinis ir naudotojai

--- ISPagrindai/settings.py
+++ ISPagrindai/settings.py
@@ -118,7 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+# Base URL for serving static files
+STATIC_URL = "/static/"
+
+# During development (optional but useful)
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # global static folder (optional)
+]
+
+# Where files will be collected when you run `collectstatic`
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
