58d29d0959ac12bd57a78d90153f2f4b5f37e3c7
Gytis Kaulakis <42701017+GytisKau@users.noreply.github.com>
2025-11-15 22:24:34 +0200

    User navigation

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,7 @@ app_name = "users"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    # path("create/", views.createUser, name="createConcert"),
+    # path("edit/", views.editUser, name="editConcert"),
+    path("detail/<int:user_id>", views.userDetail, name="userDetail"),
 ]
